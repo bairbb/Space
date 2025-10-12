d76748bf846562e0e0c7cb848f5a8b5a47465280
bairbb <qiukbb@gmail.com>
2025-10-12 19:15:31 +0800

    fix: admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,7 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationItem;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -56,6 +57,11 @@ class AdminPanelProvider extends PanelProvider
                 Authenticate::class,
             ])
             ->topNavigation()
-            ->viteTheme('resources/css/filament/admin/theme.css');
+            ->viteTheme('resources/css/filament/admin/theme.css')
+            ->navigationItems([
+                NavigationItem::make('Вернуться на сайт')
+                    ->url('districts')
+                    ->sort(10),
+            ]);
     }
 }
